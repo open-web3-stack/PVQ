f46f3f1c278892a957101961735887e9b1ee1c10
indirection42 <jiyuanz95@gmail.com>
2025-06-26 12:54:06 +0800

    improve error handling in std env
    fix thiserror #[from]

--- pvq-executor/src/error.rs
+++ pvq-executor/src/error.rs
@@ -1,28 +1,22 @@
 use pvq_primitives::PvqError;
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum PvqExecutorError<UserError> {
+    #[error("Invalid PVQ program format")]
     InvalidProgramFormat,
+    #[error("Memory access error: {0}")]
     MemoryAccessError(polkavm::MemoryAccessError),
     // Extract from the PVM CallError
+    #[error("Trap")]
     Trap,
     // Extract from the PVM CallError
+    #[error("Not enough gas")]
     NotEnoughGas,
     // Usually a custom error type from the extension system definition
+    #[error("User error: {0}")]
     User(UserError),
     // Other errors directly from the PVM
+    #[error("Other PVM error: {0}")]
     OtherPvmError(polkavm::Error),
-}
-
-impl<UserError> From<polkavm::MemoryAccessError> for PvqExecutorError<UserError> {
-    fn from(err: polkavm::MemoryAccessError) -> Self {
-        Self::MemoryAccessError(err)
-    }
-}
-
-impl<UserError> From<polkavm::Error> for PvqExecutorError<UserError> {
-    fn from(err: polkavm::Error) -> Self {
-        Self::OtherPvmError(err)
-    }
 }
 
 impl<UserError> From<polkavm::CallError<UserError>> for PvqExecutorError<UserError> {
@@ -36,6 +30,33 @@ impl<UserError> From<polkavm::CallError<UserError>> for PvqExecutorError<UserErr
     }
 }
 
+impl<UserError> From<polkavm::Error> for PvqExecutorError<UserError> {
+    fn from(e: polkavm::Error) -> Self {
+        Self::OtherPvmError(e)
+    }
+}
+
+impl<UserError> From<polkavm::MemoryAccessError> for PvqExecutorError<UserError> {
+    fn from(e: polkavm::MemoryAccessError) -> Self {
+        Self::MemoryAccessError(e)
+    }
+}
+
+#[cfg(feature = "std")]
+impl<UserError: core::fmt::Debug> From<PvqExecutorError<UserError>> for PvqError {
+    fn from(e: PvqExecutorError<UserError>) -> PvqError {
+        match e {
+            PvqExecutorError::InvalidProgramFormat => "Invalid PVQ program format".to_string(),
+            PvqExecutorError::MemoryAccessError(_) => "Memory access error".to_string(),
+            PvqExecutorError::Trap => "Trap".to_string(),
+            PvqExecutorError::NotEnoughGas => "Not enough gas".to_string(),
+            PvqExecutorError::User(user_error) => format!("Host call error: {user_error:?}"),
+            PvqExecutorError::OtherPvmError(pvm_error) => format!("Other error: {pvm_error:?}"),
+        }
+    }
+}
+
+#[cfg(not(feature = "std"))]
 impl<UserError> From<PvqExecutorError<UserError>> for PvqError {
     fn from(e: PvqExecutorError<UserError>) -> PvqError {
         match e {
