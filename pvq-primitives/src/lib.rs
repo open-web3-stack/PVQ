0bf6b650e02a34d7ba740f91f89e5381ffb2b6a5
indirection42 <jiyuanz95@gmail.com>
2025-06-26 12:54:06 +0800

    improve error handling in std env
    fix thiserror #[from]

--- pvq-primitives/src/lib.rs
+++ pvq-primitives/src/lib.rs
@@ -3,13 +3,19 @@
 extern crate alloc;
 
 use alloc::vec::Vec;
+#[cfg(not(feature = "std"))]
 use parity_scale_codec::{Decode, Encode};
+#[cfg(not(feature = "std"))]
 use scale_info::TypeInfo;
 
 pub type PvqResult = Result<PvqResponse, PvqError>;
 
 pub type PvqResponse = Vec<u8>;
 
+#[cfg(feature = "std")]
+pub type PvqError = String;
+
+#[cfg(not(feature = "std"))]
 #[derive(Debug, Clone, PartialEq, Eq, Encode, Decode, TypeInfo)]
 pub enum PvqError {
     FailedToDecode,
